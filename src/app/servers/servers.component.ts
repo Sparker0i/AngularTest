23b80808015f50730ef0dd95ccebd6e08a33bbee
Aaditya Menon <aaditya@sparker0i.me>
2022-04-30 22:11:17 +0530

    Event Binding to Bind TS code and variables with HTML template

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -2,13 +2,24 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: '.app-servers',
-  template: '<app-server></app-server><app-server></app-server>'
+  templateUrl: './servers.component.html'
 })
 export class ServersComponent implements OnInit {
+  allowNewServer = false;
+  serverCreationStatus = "No Server was created";
 
-  constructor() { }
+  constructor() {
+    setTimeout(() => {
+      this.allowNewServer = true;
+    }, 2000)
+  }
 
   ngOnInit(): void {
+
+  }
+
+  onCreateServer() {
+    this.serverCreationStatus = "Server was Created!"
   }
 
 }
